d52208f296057ac4109b91303c466f5ccfb0bb6a
Junhao <junhaolicareers@gmail.com>
2020-07-05 23:07:40 -0700

    init spring boot project

---
+++ src/test/java/com/ljh516/entertainment/entertainment/music/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljh516.entertainment.entertainment.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
