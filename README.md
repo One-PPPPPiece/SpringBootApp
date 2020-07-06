9130d51d1feacea3b3a67db1de7e25a7b0221cb5
Junhao <junhaolicareers@gmail.com>
2020-07-06 00:43:40 -0700

    Added controller

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 ## Local setup
 * __build app:__ ```./gradlew build --continue```
-
+* __check app test coverage:__ ```./gradlew test jacocoTestCoverageVerification```
+* __run app in local:__ ```run MusicApplication.java in your idea or cd /entertainment.music/applications/music-server/build/libs and java -jar music-server-1.0.0.jar```
+* __localhost endpoint:__ ```http://localhost:8080/music/all```
+* __Formatting code:__ ```./gradlew goJF``` [Link](https://github.com/sherter/google-java-format-gradle-plugin)
+ 
 
 Inspired by https://github.com/barinek/appcontinuum/tree/master
--- components/music-service/src/main/java/com/ljh516/entertainment/music/MusicController.java
+++ components/music-service/src/main/java/com/ljh516/entertainment/music/MusicController.java
@@ -1,15 +1,20 @@
 package com.ljh516.entertainment.music;
 
 import javax.ws.rs.core.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/entertainment/music")
+@RestController("/music")
 public class MusicController {
 
-    @GetMapping(path = "/music/all")
-    public ResponseEntity getAllMusics() {
+    Logger log = LoggerFactory.getLogger(MusicController.class);
+
+    @GetMapping(path = "/music/all", produces = "application/json")
+    public ResponseEntity getAllMusic() {
+        log.info("inside getAllMusic");
         return ResponseEntity.status(Response.Status.ACCEPTED.getStatusCode()).body("Music Body");
     }
 }
